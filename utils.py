365b16ce954738766cce512fce1cdaac1c4c668e
Abrar Rahman Haqeem Bachrie <abrarrahmanhakim@gmail.com>
2026-09-04 23:54:53 +0800

    initial commit

---
+++ utils.py
@@ -0,0 +1,23 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "146f70c0-8ce0-4fe8-8bab-1d3d06e7cef2",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "",
+   "name": ""
+  },
+  "language_info": {
+   "name": ""
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
